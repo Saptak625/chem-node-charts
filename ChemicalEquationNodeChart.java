9f5c22cbd391f8085c2c204ee0ad4609b9a75fa5
Saptak625 <66028584+Saptak625@users.noreply.github.com>
2020-08-24 14:28:29 -0400

    Add files via upload

--- ChemicalEquationNodeChart.java
+++ ChemicalEquationNodeChart.java
@@ -9,8 +9,6 @@ import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-import static com.saptakdas.misc.Probability.getCombinations;
-
 /**
  * This class can be used to create a graph of the balancing of a chemical equation.
  * @author Saptak Das
@@ -66,6 +64,55 @@ public class ChemicalEquationNodeChart {
 
         //Start of Node Chart Generation
         makeNodeChart(variableSpace,elements, reactants, products);
+    }
+
+    /**
+     * @param iterable LinkedList of possible combination values
+     * @param k int value of length of solution
+     * @return LinkedList of combinations
+     */
+    public static LinkedList getCombinations(LinkedList iterable, int k){
+        var n=iterable.size();
+        var storageList=new LinkedList();
+        var list=new LinkedList();
+        //Prepping
+        for (int i=0; i<k; i++){
+            list.addLast(i+1);
+        }
+        //Algorithm
+        boolean changed;
+        while (((int) list.get(0))<(n-k+2)){
+            changed=false;
+            for (int i=0; i<list.size(); i++) {
+                var index=list.size()-i-1;
+                if ((int) list.get(index) > n) {
+                    list.set(index-1,(int) list.get(index-1)+1);
+                    for (int j=index; j<list.size(); j++){
+                        list.set(j, (int) list.get(j-1)+1);
+                    }
+                    changed=true;
+                }
+            }
+            if (!changed) {
+                storageList.addLast(indexesToCombinations(list, iterable));
+                list.set(list.size() - 1, (int) list.get(list.size() - 1) + 1);
+            }
+        }
+        return storageList;
+    }
+
+    /**
+     * @param list LinkedList
+     * @param iterable LinkedList
+     * @return LinkedList of combinations
+     */
+    private static LinkedList indexesToCombinations(LinkedList<Integer> list, LinkedList iterable){
+        var returnlist=new LinkedList();
+        for (int index:list) {
+            int i=index-1;
+            returnlist.addLast(iterable.get(i));
+        }
+        return returnlist;
     }
 
     /**
